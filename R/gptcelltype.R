c434645f8a4190a027e92dce004d5b98e1d2a4d1
Payne-Wang <44337721+Payne-Wang@users.noreply.github.com>
2024-09-08 21:36:19 +0800

    Update gptcelltype.R

--- R/gptcelltype.R
+++ R/gptcelltype.R
@@ -29,8 +29,10 @@
 #' prompt <- gptcelltype(all.markers, tissuename='human PBMC')
 #' return(prompt)
 
-gptcelltype <- function(input, tissuename=NULL, model='gpt-4', topgenenumber = 10,base_url=NULL) {
-  OPENAI_API_KEY <- Sys.getenv("OPENAI_API_KEY")
+gptcelltype <- function(input, tissuename=NULL, model='gpt-4', topgenenumber = 10,base_url=NULL,api_key=NULL) {
+  if(!is.null(Sys.getenv("OPENAI_API_KEY"))){
+    OPENAI_API_KEY <- Sys.getenv("OPENAI_API_KEY")
+  }else{OPENAI_API_KEY <- api_key}
   if (OPENAI_API_KEY == "") {
     print("Note: OpenAI API key not found: returning the prompt itself.")
     API.flag <- 0
